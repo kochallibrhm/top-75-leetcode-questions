6fc85a8a69436f28376ff0601acbfa368e8e9192
Halil ibrahim Koc <kochallibrhm@gmail.com>
2022-02-02 19:56:00 +0300

    not working!!!! need to fix

---
+++ DynamicProgramming/WordBreak.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function(s, wordDict) {
+    const len = s.length;
+    let dp = Array.from(s.length + 1).fill(false);
+    dp[len] = true;
+  
+    for(let i = len; len < -1; i--){
+        for(let j = 0; j < i; j++){
+            if(i + wordDict.length <= len && s.substring(i, i + wordDict[j].length).includes(wordDict[j]))
+                dp[i] = dp[i + wordDict.length];
+            if(dp[i])
+                break;
+        }
+    }
+  
+    return dp[0];
+};
