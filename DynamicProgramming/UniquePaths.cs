2f915eefc8f9fbb53c91fbb881b62384794c43d5
Halil ibrahim Koc <kochallibrhm@gmail.com>
2023-01-29 01:23:09 +0300

    Create UniquePaths.cs

---
+++ DynamicProgramming/UniquePaths.cs
@@ -0,0 +1,23 @@
+// Runtime: 31 ms Beats 42.24% || Memory: 26.8 MB Beats 19.30%
+
+public class Solution {
+    public int UniquePaths(int m, int n) {
+        int[,] dp = new int[m,n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 || j == 0)
+                    dp[i, j] = 1;
+                else
+                    dp[i, j] = dp[i-1, j] + dp[i, j-1];
+            }
+        }
+        return dp[m-1, n-1];
+    }
+}
+/*
+There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+The test cases are generated so that the answer will be less than or equal to 2 * 109.
+*/
