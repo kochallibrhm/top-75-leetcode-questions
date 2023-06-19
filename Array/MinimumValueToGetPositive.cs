c52cb33da5cc0a61d22bcd40f2cae892cd4bfb0f
Halil ibrahim Koc <kochallibrhm@gmail.com>
2023-06-19 23:23:43 +0300

    Create MinimumValueToGetPositive.cs

---
+++ Array/MinimumValueToGetPositive.cs
@@ -0,0 +1,34 @@
+/*Given an array of integers nums, you start with an initial positive value startValue.
+
+In each iteration, you calculate the step by step sum of startValue plus elements in nums (from left to right).
+
+Return the minimum positive value of startValue such that the step by step sum is never less than 1.
+
+ 
+
+Example 1:
+
+Input: nums = [-3,2,-3,4,2]
+Output: 5
+Explanation: If you choose startValue = 4, in the third iteration your step by step sum is less than 1.
+step by step sum
+startValue = 4 | startValue = 5 | nums
+  (4 -3 ) = 1  | (5 -3 ) = 2    |  -3
+  (1 +2 ) = 3  | (2 +2 ) = 4    |   2
+  (3 -3 ) = 0  | (4 -3 ) = 1    |  -3
+  (0 +4 ) = 4  | (1 +4 ) = 5    |   4
+  (4 +2 ) = 6  | (5 +2 ) = 7    |   2
+*/
+// Runtime 86 ms Beats 61.82%
+// Memory 38.1 MB Beats 68.48%
+public class Solution {
+    public int MinStartValue(int[] nums) {
+        var balance = 0;
+        var min = 0;
+        foreach(int num in nums){
+            balance += num;
+            min = Math.Min(min, balance);
+        }
+        return 1-min;
+    }
+}
