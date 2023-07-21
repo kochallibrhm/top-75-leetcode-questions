8cfc86cf9401cbc94caa3e02849f893cc88fe3ae
Halil ibrahim Koc <kochallibrhm@gmail.com>
2023-07-21 21:33:48 +0300

    Create ReversedLinkedList.cs
    Solution of reversed linked list

---
+++ LinkedList/ReversedLinkedList.cs
@@ -0,0 +1,31 @@
+/*
+ Given the head of a singly linked list, reverse the list, and return the reversed list.
+ 
+Runtime Details 76ms BEATS 97.13% OF USERS WITH C#
+
+Memory Details 39.10mb Beats 48.83%of users with C#
+
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode current = head;
+        ListNode previous = null;
+        ListNode next = null;
+        while(current != null){
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+}
