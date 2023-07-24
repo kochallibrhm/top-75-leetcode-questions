5e9929fa0b2a649f070bdafbfbcd1d4c5a42feed
Halil ibrahim Koc <kochallibrhm@gmail.com>
2023-07-25 01:31:46 +0300

    Update RemoveElement.cs

--- Array/RemoveElement.cs
+++ Array/RemoveElement.cs
@@ -37,3 +37,11 @@ public class Solution {
         return notIncluding.Count();
     }
 }
+/*
+Example 1:
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+*/
